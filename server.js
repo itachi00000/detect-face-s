a93d2e86cb18668f2aa20b392903fbc79fcc1f4a
itachi00000 <>
2020-04-08 01:59:31 +0800

    1. remove public /folder 2. modify the connection using heroku instruction

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: '1234',
-    database: 'smart-brain-db'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
