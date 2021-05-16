1ccc48be367423f01ec5f04eb2b0a1fecf6ff21c
itachi0 <>
2021-05-16 18:28:03 +0800

    edit signin to handle req w/ or w/o jwt-token

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,33 +1,36 @@
 const jwt = require('jsonwebtoken');
 
-const redisClient = require('../auth').redisClient;
-// const redis = require('redis');
-
-// // (port, host)
-// const redisClient = redis.createClient(6379, '127.0.0.1');
-// // redisClient.selected_db = 1;
-
-// redisClient.on('connect', ()=>{
-//   console.log('<< connected to REDIS!')
-// })
-
-// // error
-// redisClient.on('error', (error) => {
-//   console.error('error-redis', error);
-// });
-
-// redisClient.set('foo', 'Ok');
-
-// redisClient.get('foo', (err, reply) => {
-//   console.log(reply.toString());
-// });
+// const redisClient = require('../auth').redisClient;
 
 const handleSignin = (req, res, db, bcrypt) => {
+  const { authorization } = req.headers
+
+// if authorized, Bearer <token>
+if( authorization ){
+   const decoded = jwt.verify(authorization.replace('Bearer ', ''), process.env.JWT_SECRET)
+   
+   return db
+    .select('*')
+    .from('users')
+    .where('email', '=', decoded.email)
+    .then((data) => {
+
+      if (!data.length) {
+        throw Error('no user found');
+      }
+
+      return res.json(data[0])
+    })
+    .catch(console.error)
+  }
+
+// if not authorized
+
   const { email, password } = req.body;
 
   // validation
   if (!email || !password) {
-    return Promise.reject('incorrect form submission');
+    throw Error('incorrect form submission');
   }
 
   // get by email
@@ -36,62 +39,73 @@ const handleSignin = (req, res, db, bcrypt) => {
     .from('login')
     .where('email', '=', email)
     .then((data) => {
+      
       // exist?
-      if (!data[0]) {
-        return Promise.reject(Error('no user'));
+      if (!data.length) {
+        throw Error('no user');
       }
 
       // validating password
       const isValid = bcrypt.compareSync(password, data[0].hash);
 
-      if (isValid) {
-        return db
-          .select('*')
-          .from('users')
-          .where('email', '=', email)
-          .then((user) => user[0])
-          .catch((err) => Promise.reject(err));
-      } else {
-        return Promise.reject(Error('wrong credentials'));
+      if (!isValid) {
+        throw Error('wrong credentials');
       }
+
+      const token = jwt.sign({ email: data[0].email }, process.env.JWT_SECRET, {
+        expiresIn: '1h',
+      });
+
+      return db
+        .select('*')
+        .from('users')
+        .where('email', '=', email)
+        .then((users) => {
+          return res.json({
+            success: true,
+            userId: users[0].id,
+            token
+          });
+        })
+        .catch((err) => res.status(400).json(err.toString()));
     })
-    .catch((err) => Promise.reject(err));
+    .catch((err) => res.status(400).json(err.toString()));
 };
 
-const getAuthTokenId = (req, res) => {
-  const { authorization } = req.headers;
-  console.log('processing authorize');
+// const getAuthTokenId = (req, res) => {
+//   const { authorization } = req.headers;
+//   console.log('processing authorize');
 
-  return redisClient.get(authorization, (err, reply) => {
-    if (err || !reply) {
-      return res.status(400).json('Unauthorized');
-    }
-    console.log('authorized');
-    return res.json({ id: reply });
-  });
-};
+//   return redisClient.get(authorization, (err, reply) => {
+//     if (err || !reply) {
+//       return res.status(400).json('Unauthorized');
+//     }
+//     console.log('authorized');
+//     return res.json({ id: reply });
+//   });
+// };
 
-const setToken = (token, id) => {
-  // .set(key, value)
-  // promisify it
-  return Promise.resolve(redisClient.set(token, id));
-};
+// const setToken = (token, id) => {
+//   // .set(key, value)
+//   // promisify it
+//   return Promise.resolve(redisClient.set(token, id));
+// };
 
-const createSessions = (user) => {
-  //  jwt
+// const createSessions = (user) => {
+//   //  jwt
 
-  const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
-    expiresIn: '2d'
-  });
+//   const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
+//     expiresIn: '2d'
+//   });
 
-  return setToken(token, user.id)
-    .then(() => ({
-      success: true,
-      userId: user.id,
-      token
-    }))
-    .catch((err) => console.error(err));
-};
+//   return setToken(token, user.id)
+//     .then(() => ({
+//       success: true,
+//       userId: user.id,
+//       token
+//     }))
+//     .catch((err) => console.error(err));
+// };
 
 const signinAuthentication = (req, res, db, bcrypt) => {
   const { authorization } = req.headers;
@@ -101,22 +115,27 @@ const signinAuthentication = (req, res, db, bcrypt) => {
   return authorization
     ? getAuthTokenId(req, res)
     : handleSignin(req, res, db, bcrypt)
-        .then((data) => {
-          // console.log('data', data);
-          if (data.id && data.email) {
-            // if logging in
-            // return .then((session) =>
-            return createSessions(data);
+        .then((user) => {
+          if (!user.id || !user.email) {
+            return Promise.reject(Error('no user id or email'));
           }
-          return Promise.reject(Error('no user id or email'));
-        })
-        .then((session) => {
-          console.log('session', session);
-          return res.json(session);
+
+          const token = jwt.sign(
+            { email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '2d' }
+          );
+
+          return res.json({
+            success: true,
+            userId: user.id,
+            token
+          });
         })
         .catch((err) => res.status(400).json(err.toString()));
 };
 
 module.exports = {
-  signinAuthentication
+  signinAuthentication,
+  handleSignin
 };
